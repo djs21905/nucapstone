5b8d93e14b515dd05e910853aa7ba3817537a887
djs21905 <djs21905@gmail.com>
2021-01-19 14:50:53 -0500

    Add files via upload
    dynamic news posts

--- myrepolib/repomod.py
+++ myrepolib/repomod.py
@@ -1,13 +1,38 @@
 from flask import Flask, render_template, request
 from google.cloud import automl_v1beta1
 from newspaper import Article
+from newspaper import Config
+from GoogleNews import GoogleNews
 
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET", "POST"])
 def hello():
-    return render_template('hello.html') 
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+    config = Config()
+    config.browser_user_agent = user_agent
+    googlenews=GoogleNews(start='01/19/2021',end='01/19/2021')
+    googlenews.search('news')
+    result=googlenews.result()
+
+
+    trun = result[0:3]
+
+    titles = []
+    outlet = [] 
+    date = []
+    links = []
+    for item in trun:
+        titles.append(item['title'])
+        outlet.append(item['media'])
+        date.append(item['date'])
+        links.append(item['link'])
+    
+
+
+    return render_template('hello.html',result= zip(titles,outlet,date,links)) 
+
 
 
 
@@ -23,7 +48,7 @@ def test():
         text = article.text
         result= [text]
         labels = ['Text']
-
+      
 
             
     return render_template('test.html', result= zip(result,labels))  #, prediction= round(pred_value * 100,2))
